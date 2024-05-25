d897e94b7bce2fc1ecdb8f919a6dbf1298c4f178
=Dominik Schrenk <dsschrenk@gmail.com>
2024-05-25 16:44:49 +0200

    Added 2 more tests and they are green too

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -4,14 +4,25 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual("foo");
   });
-  
   
   it("quality decreases by 1", function() { //Test case 2
     const gildedRose = new Shop([ new Item("foo", 1, 1) ]); //creates shop with item foo (sellIn = 1, quality = 1)
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toEqual(0); //quality des ersten Items muss 0 sein (pass)
+  });
+
+  it("quality cannot be negative", function() { //Test case 3
+    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(0); 
+  });
+
+  it("'Aged Brie' increases in quality", function() { //Test case 4
+    const gildedRose = new Shop([ new Item("Aged Brie", 1, 0) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(1);
   });
   
 });
